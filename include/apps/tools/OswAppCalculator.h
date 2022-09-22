dc8f097019fcdae1eea47445218ddc5b112cc4d4
ich42 <80181603+ich42@users.noreply.github.com>
2022-09-22 18:11:47 +0200

    add refined versions of OswAppCalculator and OswAppFlashLight

---
+++ include/apps/tools/OswAppCalculator.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <osw_hal.h>
+#include <osw_ui.h>
+#include "osw_app.h"
+
+
+class OswAppCalculator : public OswApp {
+  public:
+    OswAppCalculator(void) {
+        ui = OswUI::getInstance();
+    };
+    virtual void setup() override; 
+    virtual void loop() override; //checks the cursor position (iNum) and starts the appropriate function
+    virtual void stop() override;
+    void draw(); //draws the ui
+    void setPNum1(); //gets the input that tells if num1 is positive or negative
+    void setNum1(int num1[]); //is used to fill the array that will become num1
+    void setOP(); //gets the input to set the operator
+    void setPNum2(); //gets the input that tells if num2 is positive or negative
+    void setNum2(int num2[]); //is used to fill the array that will become num2
+    void equal(); //calculates the output 
+    void clear(); //clears num1, num2, and the operator, only the result stays 
+
+
+    ~OswAppCalculator() {};
+
+  private:
+    OswUI* ui;
+
+};
---
+++ include/apps/tools/OswAppFlashLight.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <osw_hal.h>
+#include <osw_ui.h>
+#include "osw_app.h"
+
+
+class OswAppFlashLight : public OswApp {
+  public:
+    OswAppFlashLight(void) {
+        ui = OswUI::getInstance();
+    };
+    virtual void setup() override; //sets the brightness to the current brightness (else the brighntess would say it was 0 for some reason) 
+    virtual void loop() override; //checks for button presses and turns the flashlight on/off
+    virtual void stop() override; //sets the brightness to the initial value 
+    void drawBG(bool on); //draws the app name when the flashlight is turned off and a circle 
+    void brightness(bool on); // shows the brighntess when the flashlight is turned on
+    
+    void storeBrightness(); //stores the brightness to set it to the initial value it was when the app started (usefull in the dark)
+    
+    ~OswAppFlashLight() {};
+
+  private:
+    OswUI* ui;
+
+};
