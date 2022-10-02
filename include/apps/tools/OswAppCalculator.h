14a085444dd75528254b544a0fc1ade7d3e95b46
ich42 <80181603+ich42@users.noreply.github.com>
2022-10-02 18:05:41 +0200

    make functions private

--- include/apps/tools/OswAppCalculator.h
+++ include/apps/tools/OswAppCalculator.h
@@ -13,6 +13,9 @@ class OswAppCalculator : public OswApp {
     virtual void setup() override;
     virtual void loop() override; //checks the cursor position (iNum) and starts the appropriate function
     virtual void stop() override;
+    ~OswAppCalculator() {};
+  private:
+    OswUI* ui;
     void draw(); //draws the ui
     void setPNum1(); //gets the input that tells if num1 is positive or negative
     void setNum1(int num1[]); //is used to fill the array that will become num1
@@ -21,7 +24,4 @@ class OswAppCalculator : public OswApp {
     void setNum2(int num2[]); //is used to fill the array that will become num2
     void equal(); //calculates the output
     void clear(); //clears num1, num2, and the operator, only the result stays
-    ~OswAppCalculator() {};
-  private:
-    OswUI* ui;
 };
--- include/apps/tools/OswAppFlashLight.h
+++ include/apps/tools/OswAppFlashLight.h
@@ -10,13 +10,13 @@ class OswAppFlashLight : public OswApp {
     OswAppFlashLight(void) {
         ui = OswUI::getInstance();
     };
+    ~OswAppFlashLight() {};
+  private:
+    OswUI* ui;
     virtual void setup() override; //calls storeBrighntess();
     virtual void loop() override; //checks for button presses and turns the flashlight on/off
     virtual void stop() override; //sets the brightness to the initial value
     void drawBG(bool on); //draws the app name when the flashlight is turned off and a circle
     void brightness(bool on); // shows the brighntess when the flashlight is turned on
     void storeBrightness(); //stores the brightness to set it to the initial value it was when the app started (usefull in the dark)
-    ~OswAppFlashLight() {};
-  private:
-    OswUI* ui;
 };
