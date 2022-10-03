c93725a68bea6e7617923ae1e40e3b0f1d031733
ich42 <80181603+ich42@users.noreply.github.com>
2022-10-03 19:05:56 +0200

    remove git merge lines

--- include/apps/tools/OswAppFlashLight.h
+++ include/apps/tools/OswAppFlashLight.h
@@ -16,10 +16,6 @@ class OswAppFlashLight : public OswApp {
     virtual void setup() override;
     virtual void loop() override; //checks for button presses and turns the flashlight on/off
     virtual void stop() override;
-<<<<<<< HEAD
     void draw(short flashlightBrightness, bool on); 
-=======
-    void draw(bool on);
->>>>>>> 505875b0ef8b4acc39692e585ba10b923335750c
 
 };
